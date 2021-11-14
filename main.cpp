c81847daae4097724552c567ced71f5ad59c9a2e
Ettore002 <91873786+Ettore002@users.noreply.github.com>
2021-11-14 20:16:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,22 +2,17 @@
 using namespace std;
 
 int main() {
-   int n;
-   cin>> n;
-   int x=1;y=1, z=0;
-   if(n>3){
-      for(int i=0;i<n;i++){
-         z=x+y;
-         cout<< x<< endl;
-         if( x<=n){
-            x=y;
-            y=z;
-         }
-      }
-   }else{
-      for(int i=-1;i<n;i++){
-         z=x+y;
-         y=z;
+   int n=0,x=1,y=1,z=0;
+   cin>>n;
+   if(n>0){
+      cout <<x<< endl;
+   }
+   while(x<=n){
+      x=y+z;
+      z=y;
+      y=x;
+      if(x<=0){
+         cout<<x<<endl;
       }
    }
    return 0;
